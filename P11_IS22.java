c5fe6f95fefa8dc621e59611b66a6d6bc5230ea6
Abraham Lopez Meza <100797014+xabrvhvmx@users.noreply.github.com>
2023-09-27 01:23:21 -0600

    Add files via upload

--- P11_IS22.java
+++ P11_IS22.java
@@ -1,19 +1,14 @@
-//realice un programa que pida la edad de un usuario y me indique que es mayor de edad//
 import java.util.Scanner;
-class P11_IS22{
+
+public class P11_IS22{
 	public static void main(String[] args) {
+		int edad;
 		Scanner teclado = new Scanner(System.in);
-		int Edad;
-		System.out.println("Ingresa tu edad: ");
-		Edad = teclado.nextInt();
-        if (Edad >= 18){
-        System.out.println("Eres mayor de edad");
-        }else{
-        System.out.println("Eres menor de edad");
-        System.out.println("Raiz cuadrada de 10 " + Math.sqrt(10));
-        System.out.println("Potencia " + Math.pow(5,3));
-        System.out.println("Residuo de 10/2: " + (10 % 2));
-        System.out.println("Residuo de 11/2: " + (11 % 2));
-        }
+		System.out.println("Ingrese su edad: ");
+		edad = teclado.nextInt();
+		if (edad>=18){
+			System.out.println("Eres mayor de edad. ");}
+		if (edad<=17){
+			System.out.println("No eres mayor de edad.");}
 	}
 }
