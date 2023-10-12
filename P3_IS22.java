b703b9133fd8d0702b4287ae26313cadf6828c98
Abraham Lopez Meza <100797014+xabrvhvmx@users.noreply.github.com>
2023-10-11 18:45:02 -0600

    Delete P3_IS22.java

--- P3_IS22.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class P3_IS22{
-	public static void main(String[] args) {
-	    String nombre;
-	    Scanner teclado = new Scanner(System.in);
-	    System.out.println("Ingresa tu nombre: ");
-	    nombre = teclado.nextLine();
-	    System.out.println("Bienvenido, "+ nombre + " Esta es la clase de computacion");
-}}
