086825800c410c49133ee9389c380a421a0bbb07
Abraham Lopez Meza <100797014+xabrvhvmx@users.noreply.github.com>
2023-10-11 18:31:21 -0600

    Delete Ejercicio1_IS22.java

--- Ejercicio1_IS22.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Ejercicio1_IS22{
-	public static void main(String[] args) {
-	System.out.println("*********");
-	System.out.println("*       *");
-	System.out.println("*       *");
-	System.out.println("*********");
-	}
-}
